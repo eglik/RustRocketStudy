4b6acc42a4bd84359853f2eb21a014f9bdcbf570
EGLIK <eglik@odin.vision>
2019-09-06 11:59:10 +0900

    test: trait

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+
+
+#[derive(Debug, Clone, Copy)]
+struct Vector2 {
+    x: f32,
+    y: f32,
 }
+
+fn main() {
+    let vector = Vector2 { x: 3.0, y: 5.0 };
+    let vector2 = vector;
+
+    println!("{:?}", vector);
+}
+
+
+
+
